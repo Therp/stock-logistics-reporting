7a3cfce4972cf18a4aecbac1906d6bce85ad9932
ntsirintanis <ntsirintanis@therp.nl>
2025-01-17 15:21:55 +0100

    fixup! [UPD] stock_picking_report_cmr: more fields in report

--- stock_picking_report_cmr/models/stock_picking.py
+++ stock_picking_report_cmr/models/stock_picking.py
@@ -10,3 +10,13 @@ class StockPicking(models.Model):
         help="Code needed for CMR Report",
     )
     nature_of_goods = fields.Char()
+    effective_package_type = fields.Char(compute="_compute_effective_package_type")
+
+    def _compute_effective_package_type(self):
+        for picking in self:
+            if hasattr(picking, "package_type"):
+                picking.effective_package_type = picking.package_type
+                continue
+            picking.effective_package_type = picking.move_line_ids.mapped(
+                "result_package_id.packaging_id.name"
+            )
